a204bfb9f8e98b92707dc489ff4e0594efc7416a
novitangrh <novitasabila70@gmail.com>
2023-11-22 15:24:12 +0700

    Update DashboardPage

--- setel-app-test/src/components/DashboardPage.vue
+++ setel-app-test/src/components/DashboardPage.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container>
-        <h1 class="pl-5 my-auto display-1 font-weight-bold">Welcome, Admin!</h1>
-        <h2 class="pl-5 my-5 display-1 font-weight-bold" style="color: #B71C1C">Statistik Peminjaman Kendaraan</h2>
-        <chart-peminjaman class="my-5 pl-5" />
-        <h2 class="pl-5 mt-10 display-1 font-weight-bold" style="color: #B71C1C">Informasi Data Terkini</h2>
-        <v-row class="justify-space-between pa-5" align="center">
+        <h1 class="display-1 font-weight-bold">Welcome, Admin!</h1>
+        <h2 class="mt-5 display-1 font-weight-bold" style="color: #B71C1C">Statistik Peminjaman Kendaraan</h2>
+        <chart-peminjaman class="mt-3 mb-5" />
+        <h2 class="mt-10 mb-3 display-1 font-weight-bold" style="color: #B71C1C">Informasi Data Terkini</h2>
+        <v-row class="justify-space-between" align="center">
             <v-col v-for="(item, index) in dataKendaraan" :key="index">
                 <v-card class="mx-auto my-auto rounded-lg" color="red-darken-4" style="box-shadow: none">
                     <v-sheet class="d-flex flex-no-wrap pa-2 justify-space-between bg-red-darken-4">
@@ -13,15 +13,13 @@
                             <v-card-subtitle class="text-h6 font-weight-bold color-white">{{ item.amount
                             }}</v-card-subtitle>
                             <v-card-actions>
-                                <v-btn class="ms-2 mt-4" variant="outlined" size="small"
+                                <v-btn class="ms-2 mt-3" variant="outlined" size="small"
                                     @click="openDialog(index)">DETAILS</v-btn>
                                 <v-dialog v-model="dialog" width="auto">
-                                    <v-card class="my-custom-border-details my-auto justify-center" width="350"
-                                        height="230">
-                                        <v-card-title class="text-center text-h6" for="">{{ selectedItem.titleDetails
+                                    <v-card class="justify-center" width="380">
+                                        <v-card-title class="text-h5" for="">{{ selectedItem.titleDetails
                                         }}</v-card-title>
-                                        <v-sheet class="my-custom-border mx-auto justify-center rounded-lg"
-                                            width="300" height="110">
+                                        <v-sheet class="my-custom-border mx-auto justify-center rounded-lg" width="320">
                                             <div class="d-flex justify-center align-center mx-10 my-3">
                                                 <v-icon align="center" icon="mdi-bicycle" size="30"></v-icon>
                                                 <label class="mx-auto text-h6">{{ selectedItem.unitSepeda }}</label>
@@ -41,8 +39,8 @@
                             </v-card-actions>
                         </div>
                         <v-avatar class="my-auto" size="125" rounded="0">
-                            <v-img :src="require(`@/assets/${item.imageSource}`)" alt="Item Image" width="80"
-                                height="80"></v-img>
+                            <v-img :src="require(`@/assets/${item.imageSource}`)" alt="Item Image" width="100"
+                                height="100"></v-img>
                         </v-avatar>
                     </v-sheet>
                 </v-card>
@@ -51,7 +49,7 @@
 
         <!-- Informasi Shelter -->
         <!-- <h2 class="pl-5 display-1 font-weight-bold" style="color: #B71C1C">Shelter Setel</h2> -->
-        <v-row class="px-5 my-3 justify-space-between" align="center">
+        <v-row class="my-3 justify-space-between" align="center">
             <v-col v-for="(image, index) in dataShelter" :key="index" :cols="12" :sm="12" :md="6">
                 <v-card class="my-custom-border py-2" style="box-shadow: none">
                     <v-row class="px-5 py-3">
@@ -62,24 +60,24 @@
                             </v-sheet>
                         </v-col>
                         <v-col cols="6" class="d-flex flex-column">
-                            <v-card class="h-100 bg-red-darken-4 mb-4">
+                            <v-card class="h-100 bg-red-darken-4">
                                 <v-card-items class="h-50 d-flex flex-row justify-center align-center pa-5">
-                                    <v-icon icon="mdi-bicycle" size="x-large" class="mr-3"></v-icon>
+                                    <v-icon icon="mdi-bicycle" size="x-large" class="mr-5"></v-icon>
                                     <label class="text-center text-sm-body-1 font-weight-bold">{{ image.unit }}</label>
                                 </v-card-items>
                                 <v-divider class="justify-center border-opacity-100" :thickness="2"></v-divider>
                                 <v-card-items class="h-50 d-flex flex-row justify-center align-center pa-5">
-                                    <v-icon icon="mdi-scooter" size="x-large" class="mr-3"></v-icon>
+                                    <v-icon icon="mdi-scooter" size="x-large" class="mr-5"></v-icon>
                                     <label class="text-center text-sm-body-1 font-weight-bold">{{ image.unit }}</label>
                                 </v-card-items>
                             </v-card>
                         </v-col>
                     </v-row>
 
-                    <v-card-title class="font-weight-bold text-center text-red-darken-4 text-h5 mt-1" width="500"
-                        style="box-shadow: none" for="">{{ image.title }}</v-card-title>
-                    <v-card-text class="font-weight-bold text-center text-red-darken-4 text-subtitle-1" width="500"
-                        style="box-shadow: none" for="">{{ image.title2 }}</v-card-text>
+                    <v-card-title class="font-weight-bold text-center text-red-darken-4 text-h5" style="box-shadow: none"
+                        for="">{{ image.title }}</v-card-title>
+                    <v-card-text class="font-weight-bold text-center text-red-darken-4 text-h6" style="box-shadow: none"
+                        for="">{{ image.title2 }}</v-card-text>
                 </v-card>
             </v-col>
         </v-row>
